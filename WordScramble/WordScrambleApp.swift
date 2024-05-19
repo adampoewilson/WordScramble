8f60b5a20f670641b512fcfc8f4c02bf8232a2ec
adampoewilson <adam62385@gmail.com>
2024-05-18 21:22:58 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Adam Wilson on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
